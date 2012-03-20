2fd60e79de975c46b68f88083d20738bd517bbd1
Tim Krones <t.krones@gmx.net>
2012-03-20 22:01:33 +0100

    Group LaTeX-related customizations in a single section

--- init.el
+++ init.el
@@ -3,8 +3,6 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(TeX-command-list (quote (("TeX" "%(PDF)%(tex) %`%S%(PDFout)%(mode)%' %t" TeX-run-TeX nil (plain-tex-mode texinfo-mode ams-tex-mode) :help "Run plain TeX") ("LaTeX" "%`%l%(mode)%' %t" TeX-run-TeX nil (latex-mode doctex-mode) :help "Run LaTeX") ("Makeinfo" "makeinfo %t" TeX-run-compile nil (texinfo-mode) :help "Run Makeinfo with Info output") ("Makeinfo HTML" "makeinfo --html %t" TeX-run-compile nil (texinfo-mode) :help "Run Makeinfo with HTML output") ("AmSTeX" "%(PDF)amstex %`%S%(PDFout)%(mode)%' %t" TeX-run-TeX nil (ams-tex-mode) :help "Run AMSTeX") ("ConTeXt" "texexec --once --texutil %(execopts)%t" TeX-run-TeX nil (context-mode) :help "Run ConTeXt once") ("ConTeXt Full" "texexec %(execopts)%t" TeX-run-TeX nil (context-mode) :help "Run ConTeXt until completion") ("BibTeX" "bibtex %s" TeX-run-BibTeX nil t :help "Run BibTeX") ("View" "%V" TeX-run-discard t t :help "Run Viewer") ("Print" "%p" TeX-run-command t t :help "Print the file") ("Queue" "%q" TeX-run-background nil t :help "View the printer queue" :visible TeX-queue-command) ("File" "%(o?)dvips %d -o %f " TeX-run-command t t :help "Generate PostScript file") ("Index" "makeindex %s" TeX-run-command nil t :help "Create index file") ("Check" "lacheck %s" TeX-run-compile nil (latex-mode) :help "Check LaTeX file for correctness") ("Spell" "(TeX-ispell-document \"\")" TeX-run-function nil t :help "Spell-check the document") ("Clean" "TeX-clean" TeX-run-function nil t :help "Delete generated intermediate files") ("Clean All" "(TeX-clean t)" TeX-run-function nil t :help "Delete generated intermediate and output files") ("Other" "" TeX-run-command t t :help "Run an arbitrary command") ("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))))
- '(TeX-electric-sub-and-superscript t)
  '(column-number-mode t)
  '(confirm-nonexistent-file-or-buffer nil)
  '(desktop-save-mode t)
@@ -233,6 +231,109 @@
                             ("\\.odb\\'" . default))))
 (setq org-track-ordered-property-with-tag t)
 (setq org-use-speed-commands t)
+
+
+;;;;;;;;;;;;;
+;;; LaTeX ;;;
+;;;;;;;;;;;;;
+
+; Variables
+(setq TeX-command-list (quote (("TeX" "%(PDF)%(tex) %`%S%(PDFout)%(mode)%' %t"
+                                TeX-run-TeX
+                                nil
+                                (plain-tex-mode texinfo-mode ams-tex-mode)
+                                :help "Run plain TeX")
+                               ("LaTeX" "%`%l%(mode)%' %t"
+                                TeX-run-TeX
+                                nil
+                                (latex-mode doctex-mode)
+                                :help "Run LaTeX")
+                               ("Makeinfo" "makeinfo %t"
+                                TeX-run-compile
+                                nil
+                                (texinfo-mode)
+                                :help "Run Makeinfo with Info output")
+                               ("Makeinfo HTML" "makeinfo --html %t"
+                                TeX-run-compile
+                                nil
+                                (texinfo-mode)
+                                :help "Run Makeinfo with HTML output")
+                               ("AmSTeX" "%(PDF)amstex %`%S%(PDFout)%(mode)%' %t"
+                                TeX-run-TeX
+                                nil
+                                (ams-tex-mode)
+                                :help "Run AMSTeX")
+                               ("ConTeXt" "texexec --once --texutil %(execopts)%t"
+                                TeX-run-TeX
+                                nil
+                                (context-mode)
+                                :help "Run ConTeXt once")
+                               ("ConTeXt Full" "texexec %(execopts)%t"
+                                TeX-run-TeX
+                                nil
+                                (context-mode)
+                                :help "Run ConTeXt until completion")
+                               ("BibTeX" "bibtex %s"
+                                TeX-run-BibTeX
+                                nil
+                                t
+                                :help "Run BibTeX")
+                               ("View" "%V"
+                                TeX-run-discard
+                                t
+                                t
+                                :help "Run Viewer")
+                               ("Print" "%p"
+                                TeX-run-command
+                                t
+                                t
+                                :help "Print the file")
+                               ("Queue" "%q"
+                                TeX-run-background
+                                nil
+                                t
+                                :help "View the printer queue"
+                                :visible TeX-queue-command)
+                               ("File" "%(o?)dvips %d -o %f "
+                                TeX-run-command
+                                t
+                                t
+                                :help "Generate PostScript file")
+                               ("Index" "makeindex %s"
+                                TeX-run-command
+                                nil
+                                t
+                                :help "Create index file")
+                               ("Check" "lacheck %s"
+                                TeX-run-compile
+                                nil
+                                (latex-mode)
+                                :help "Check LaTeX file for correctness")
+                               ("Spell" "(TeX-ispell-document \"\")"
+                                TeX-run-function
+                                nil
+                                t
+                                :help "Spell-check the document")
+                               ("Clean" "TeX-clean"
+                                TeX-run-function
+                                nil
+                                t
+                                :help "Delete generated intermediate files")
+                               ("Clean All" "(TeX-clean t)"
+                                TeX-run-function
+                                nil
+                                t
+                                :help "Delete generated intermediate and output files")
+                               ("Other" ""
+                                TeX-run-command
+                                t
+                                t
+                                :help "Run an arbitrary command")
+                               ("XeLaTeX" "%`xelatex%(mode)%' %t"
+                                TeX-run-TeX
+                                nil
+                                t))))
+(setq TeX-electric-sub-and-superscript t)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
