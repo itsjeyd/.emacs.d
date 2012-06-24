c54e6098dbe635c702320cb198142f132fc94919
Tim Krones <t.krones@gmx.net>
2012-06-24 13:26:40 +0200

    Add support for on-the-fly syntax checking (Python) via flymake + pyflakes

--- init.el
+++ init.el
@@ -533,6 +533,19 @@ is replaced and the point is put before CHAR."
                     (list #'autopair-default-handle-action
                           #'autopair-python-triple-quote-action))))
 
+; Flymake
+(when (load "flymake" t)
+  (defun flymake-pyflakes-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list "pyflakes" (list local-file))))
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pyflakes-init)))
+(add-hook 'python-mode-hook 'flymake-mode)
+
 ; Functions
 (defun set-py-which-bufname (name)
   (interactive "sName: ")
