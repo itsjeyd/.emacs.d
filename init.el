82aecd2e6d09fc5f53b9d8a69cf627b9717d2a9a
Tim Krones <t.krones@gmx.net>
2012-09-21 10:38:16 +0200

    Have bibtex-mode automatically sort entries (awesome!)

--- init.el
+++ init.el
@@ -3,6 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(bibtex-maintain-sorted-entries t)
  '(desktop-save-mode t)
  '(tags-revert-without-query t))
 (custom-set-faces
