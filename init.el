f2cb5ffc6e40d96c618c6fc4445c08af6119b9bb
Tim Krones <t.krones@gmx.net>
2012-11-10 11:57:02 +0100

    Re-enable flymake (after installing pyflakes via Synaptic)

--- init.el
+++ init.el
@@ -260,6 +260,22 @@ is replaced and the point is put before CHAR."
 ;;; Python Development ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+; Flymake
+(when (load "flymake" t)
+  (defun flymake-pyflakes-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list "pyflakes" (list local-file))))
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pyflakes-init)))
+
+(add-hook 'python-mode-hook
+          (lambda ()
+            (unless (eq buffer-file-name nil) (flymake-mode t))))
+
 ; Subword Mode
 (add-hook 'python-mode-hook (lambda () (subword-mode 1)))
 
