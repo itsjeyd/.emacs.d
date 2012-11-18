402a3f64b94f8015dd86ecbc3775d7dd5a83fa26
Tim Krones <t.krones@gmx.net>
2012-11-18 10:14:51 +0100

    ido-ubiquitous breaks some sclang-related commands; add those to list of commands ignored by ido-ubiquitous

--- init.el
+++ init.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (tango-dark))))
+ '(custom-enabled-themes (quote (tango-dark)))
+ '(ido-ubiquitous-command-exceptions (quote (sclang-dump-interface sclang-dump-full-interface))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
