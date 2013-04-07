95fea6f6c436f7077ba0b7cb291a2ef84a38d903
Tim Krones <t.krones@gmx.net>
2013-04-07 09:29:42 +0200

    Reconfigure beginning-of-buffer and end-of-buffer for dired: They now jump to the first and last file, respectively.
    Source: whattheemacsd.com

--- init.el
+++ init.el
@@ -53,6 +53,23 @@
 (setq dired-omit-files "^\\...+$")
 (add-hook 'dired-mode-hook (lambda () (dired-omit-mode 1)))
 
+; Movement (source: whattheemacsd.com)
+(defun dired-jump-to-top ()
+  (interactive)
+  (beginning-of-buffer)
+  (dired-next-line 4))
+
+(define-key dired-mode-map
+  (vector 'remap 'beginning-of-buffer) 'dired-jump-to-top)
+
+(defun dired-jump-to-bottom ()
+  (interactive)
+  (end-of-buffer)
+  (dired-next-line -1))
+
+(define-key dired-mode-map
+  (vector 'remap 'end-of-buffer) 'dired-jump-to-bottom)
+
 ; Variables
 (setq dired-recursive-copies (quote always))
 
