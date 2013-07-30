133bd5aa6d989bfd1ae7be900fa3e3089ab4ee3e
Tim Krones <t.krones@gmx.net>
2013-07-30 21:21:29 +0200

    Direct backup files to a single designated directory.

--- init.el
+++ init.el
@@ -4,6 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (tango-dark))))
+ '(backup-directory-alist (quote (("." . "~/.emacs.d/backups"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
