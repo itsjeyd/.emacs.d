931013fb63e675e614d870e18a52763411ad179b
Tim Krones <t.krones@gmx.net>
2013-09-04 22:55:14 +0200

    Switch to generic way of loading themes.

--- init.el
+++ init.el
@@ -1,11 +1,11 @@
 (package-initialize)
+(load-theme 'wombat t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (tango-dark))))
  '(backup-directory-alist (quote (("." . "~/.emacs.d/backups"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
