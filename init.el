3629d8e6f084eaed01dbe808f9fc08b760624276
Tim Krones <t.krones@gmx.net>
2013-12-02 11:24:19 +0100

    Remove long-obsolete fix for adding current directory to head of Python's sys.path.

--- init.el
+++ init.el
@@ -179,27 +179,6 @@ is replaced and the point is put before CHAR."
 
 
 ;;;;;;;;;;;;;
-;;; Fixes ;;;
-;;;;;;;;;;;;;
-
-;; NOTE: The following fix becomes obsolete when using python-mode.el
-;; instead of python.el!
-
-;; (defun python-reinstate-current-directory ()
-;;     "When running Python, add the current directory ('') to the
-;;     head of sys.path. For security reasons, run-python passes
-;;     arguments to the interpreter that explicitly remove '' from
-;;     sys.path. This means that, for example, using
-;;     `python-send-buffer' in a buffer visiting a module's code
-;;     will fail to find other modules in the same directory.
-
-;;     Adding this function to `inferior-python-mode-hook'
-;;     reinstates the current directory in Python's search path."
-;;     (python-send-string "sys.path[0:0] = ['']"))
-;; (add-hook 'inferior-python-mode-hook 'python-reinstate-current-directory)
-
-
-;;;;;;;;;;;;;
 ;;; Fonts ;;;
 ;;;;;;;;;;;;;
 
