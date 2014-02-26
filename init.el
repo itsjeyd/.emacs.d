e437752e58ff4701a8b1741507936f2afab1db05
Tim Krones <t.krones@gmx.net>
2014-02-26 10:25:06 +0100

    Fix: Balance parens of `custom-set-variables` function form.
    They got messed up by 133bd5a.

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(backup-directory-alist (quote (("." . "~/.emacs.d/backups"))))
+ '(backup-directory-alist (quote (("." . "~/.emacs.d/backups")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
