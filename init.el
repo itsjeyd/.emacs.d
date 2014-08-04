fa5fa8b20de969ff80bcca55e30f1ad4f306e478
Tim Krones <t.krones@gmx.net>
2014-08-04 09:01:33 +0200

    Fix some small formatting inconsistencies.

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+)
 
 
 ;;;;;;;;;;;;;
@@ -443,7 +443,6 @@ HOOKS can be a list of hooks or just a single hook."
 (setq uniquify-buffer-name-style (quote forward))
 
 
-
 ;;;;;;;;;;;;;
 ;;; Modes ;;;
 ;;;;;;;;;;;;;
@@ -720,7 +719,6 @@ HOOKS can be a list of hooks or just a single hook."
 (define-key smartscan-map (kbd "s-p") 'smartscan-symbol-go-backward)
 
 
-
 ;;;;;;;;;;;;;;
 ;;; Server ;;;
 ;;;;;;;;;;;;;;
@@ -771,6 +769,7 @@ HOOKS can be a list of hooks or just a single hook."
   (interactive)
   (search-service "Urbandictionary"
                   "http://www.urbandictionary.com/define.php?term="))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Version Control ;;;
