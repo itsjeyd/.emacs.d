7d23a01aca8a345a8e4c923ff868c1870046124f
Tim Krones <t.krones@gmx.net>
2014-09-23 10:13:07 +0200

    Apply org-mode markup to strings preceded by a hyphen.

--- init.el
+++ init.el
@@ -558,6 +558,7 @@ HOOKS can be a list of hooks or just a single hook."
 ;;;;;;;;;;;;;;;;
 
 ; Emphasis
+(setcar org-emphasis-regexp-components " \t('\"{-")
 (setcar (nthcdr 1 org-emphasis-regexp-components) "\[[:alpha:]- \t.,:!?;'\")}\\")
 (setcar (nthcdr 2 org-emphasis-regexp-components) " \t\r\n\"'")
 (setcar (nthcdr 4 org-emphasis-regexp-components) 2)
