365a81dfeb40dd1a201810e7360076e88c4d9ea6
Tim Krones <t.krones@gmx.net>
2015-01-12 00:25:30 +0100

    Call `ucs-make-command` when defining key bindings instead of separately.

--- init.el
+++ init.el
@@ -355,8 +355,6 @@ line instead."
              (interactive "*p")
              (insert (make-string arg ,code-point))))))
 
-(ucs-make-command "LONG RIGHTWARDS ARROW")
-
 (defun unfill-paragraph (&optional region)
   "Takes a multi-line paragraph and turns it into a single line of text."
   (interactive)
@@ -383,7 +381,7 @@ Goes backward if ARG is negative; error if STR not found."
 (global-set-key (kbd "C-w") 'kill-region-with-arg)
 (global-set-key (kbd "M-w") 'kill-ring-save-with-arg)
 (global-set-key (kbd "M-z") 'zap-up-to-char)
-(define-key ctl-x-map (kbd "8 a") 'long-rightwards-arrow)
+(define-key ctl-x-map (kbd "8 a") (ucs-make-command "LONG RIGHTWARDS ARROW"))
 (define-key custom-keys-mode-prefix-map (kbd "f e") 'flush-empty-lines)
 (define-key custom-keys-mode-prefix-map (kbd "s u") 'sort-lines-and-uniquify)
 (define-key custom-keys-mode-prefix-map (kbd "z") 'zap-to-string)
