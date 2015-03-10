86876f50d453345f641010bc904ab7b22a6fa267
Tim Krones <t.krones@gmx.net>
2015-03-10 10:53:55 +0000

    Move `custom-set-variables` and `custom-set-faces` to separate file.

--- init.el
+++ init.el
@@ -1,18 +1,14 @@
 (package-initialize)
 (setq idle-require-idle-delay 10)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-)
+
+
+;;;;;;;;;;;;;;
+;;; Custom ;;;
+;;;;;;;;;;;;;;
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
 
 
 
@@ -124,14 +120,6 @@
 ;;;;;;;;;;;;;;;;;;;
 
 (require 'cl-lib)
-
-
-
-;;;;;;;;;;;;;;;;;;;;
-;;; Custom Stuff ;;;
-;;;;;;;;;;;;;;;;;;;;
-
-(load "wenote")
 
 
 
@@ -1547,6 +1535,14 @@ With a prefix arg, clear selective display."
 
 ; Variables
 (setq-default truncate-lines t)
+
+
+
+;;;;;;;;;;;;;;
+;;; Wenote ;;;
+;;;;;;;;;;;;;;
+
+(load "wenote")
 
 
 
