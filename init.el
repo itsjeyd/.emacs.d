b3ab9cbda5b1f9954f363c61f7f209fd677a7ab1
Tim Krones <t.krones@gmx.net>
2016-04-12 21:44:57 +0200

    helm-dash: Add AngularJS docset.

--- init.el
+++ init.el
@@ -650,7 +650,7 @@ point is on and summons `hydra-mark-lines'."
     (bind-key "C-c d" #'helm-dash-at-point)
 
     ;; Variables
-    (setq helm-dash-common-docsets '("Ansible" "BackboneJS" "Bash" "Bootstrap 3" "CSS" "CoffeeScript" "Django" "Font_Awesome" "HTML" "Haml" "JavaScript" "jQuery" "LaTeX" "MongoDB" "MySQL" "PostgreSQL" "Python 2" "RequireJS" "SQLite" "Sass" "UnderscoreJS" "Vagrant"))
+    (setq helm-dash-common-docsets '("Ansible" "AngularJS" "BackboneJS" "Bash" "Bootstrap 3" "CSS" "CoffeeScript" "Django" "Font_Awesome" "HTML" "Haml" "JavaScript" "jQuery" "LaTeX" "MongoDB" "MySQL" "PostgreSQL" "Python 2" "RequireJS" "SQLite" "Sass" "UnderscoreJS" "Vagrant"))
     (setq helm-dash-enable-debugging nil)
     (setq helm-dash-docsets-path "/storage/docsets/")
     (setq helm-dash-min-length 2))
