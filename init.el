7182e03d221d23600c8b944a550f156f9cdd91cd
Tim Krones <t.krones@gmx.net>
2016-04-12 08:34:08 +0200

    Add keyboard macro for formatting LOGBOOK entries when starting a new day.

--- init.el
+++ init.el
@@ -1544,7 +1544,12 @@ point is on and summons `hydra-mark-lines'."
     (defvar org-mode-extra-keys-map (lookup-key org-mode-map (kbd "C-c C-x")))
     (bind-keys :map org-mode-extra-keys-map
                ("C-q" . org-clock-cancel-save-buffer)
-               ("C-x" . org-clock-in-last-save-buffer))
+               ("C-x" . org-clock-in-last-save-buffer)
+               ("n" . org-clock-entry-new-day))
+
+    ;; Keyboard Macros
+    (fset 'org-clock-entry-new-day
+          [?\C-n tab ?\C-n ?\C-n ?\C-o ?\C-p ?\C-p tab ?\C-p ?\C-x ?\C-s])
 
     ;; Variables
     (setq org-clock-mode-line-total 'today)
