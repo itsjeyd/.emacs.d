2adf93358852f64bed68611c4a6b29f6fef3aad7
Tim Krones <t.krones@gmx.net>
2017-05-09 09:34:20 +0200

    projectile: Start ignoring "node_modules" directories.

--- init.el
+++ init.el
@@ -2011,6 +2011,7 @@ region, operate on a single line. Otherwise, operate on region."
   (add-to-list 'projectile-globally-ignored-directories "doxygen")
   (add-to-list 'projectile-globally-ignored-directories "include")
   (add-to-list 'projectile-globally-ignored-directories "man")
+  (add-to-list 'projectile-globally-ignored-directories "node_modules")
   (add-to-list 'projectile-globally-ignored-directories "vendor")
   (setq projectile-cache-file "~/.emacs.d/.projectile.cache")
   (setq projectile-completion-system 'helm)
