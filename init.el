2f5057481dbfe6bf14dc66dc02a9939d178a6554
Tim Krones <tim@opencraft.com>
2021-12-20 13:29:07 +0100

    org-mode: Optimize keyboard macro for "new day" clock entries.
    Avoid searching for LOGBOOK drawer which can be slow in large files.

--- init.el
+++ init.el
@@ -1428,7 +1428,7 @@ region, operate on a single line. Otherwise, operate on region."
 
     ;; Keyboard Macros
     (fset 'org-clock-entry-new-day
-          [?\C-s ?: ?L ?O ?G ?B ?O ?O ?K ?: ?\C-m ?\C-a tab ?\C-n ?\C-n ?\C-o ?\C-p ?\C-p tab ?p ?\C-x ?\C-s])
+          [?\C-n tab ?\C-n ?\C-n ?\C-o ?\C-p ?\C-p tab ?p ?\C-x ?\C-s])
 
     ;; Variables
     (setq org-clock-mode-line-total 'today)
